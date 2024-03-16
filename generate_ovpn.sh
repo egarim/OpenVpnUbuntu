f5947d9bf8b94a5b24df3fa025a8a5e0cc9da39b
Jose Ojeda <joche.ojeda@bitframeworks.com>
2024-03-16 11:53:40 -0700

    Update generate_ovpn.sh

--- generate_ovpn.sh
+++ generate_ovpn.sh
@@ -1,41 +1,43 @@
 #!/bin/bash
 
-# Set Easy-RSA path
+# Set Easy-RSA and OpenVPN directories
 EASY_RSA_DIR="/etc/openvpn/easy-rsa"
+OVPN_DIR="/etc/openvpn"
 
-# Check if Easy-RSA exists
-if [ ! -d "$EASY_RSA_DIR" ]; then
-    echo "Easy-RSA is not installed in $EASY_RSA_DIR. Please specify the correct directory."
+# Check if Easy-RSA and OpenVPN directories exist
+if [ ! -d "$EASY_RSA_DIR" ] || [ ! -d "$OVPN_DIR" ]; then
+    echo "Error: Easy-RSA or OpenVPN directory not found. Please make sure OpenVPN is installed and configured."
     exit 1
 fi
 
-# Check if client name, server IP, and server port are provided
-if [ $# -ne 3 ]; then
-    echo "Usage: $0 <client_name> <server_ip> <server_port>"
+# Check if client name is provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <client_name>"
     exit 1
 fi
 
-# Set client name, server IP, and server port
+# Set client name
 CLIENT_NAME="$1"
-SERVER_IP="$2"
-SERVER_PORT="$3"
 
-# Change directory to Easy-RSA
+# Set Easy-RSA directory
 cd "$EASY_RSA_DIR" || exit
 
 # Generate client key and certificate
 ./easyrsa gen-req "$CLIENT_NAME" nopass
 ./easyrsa sign-req client "$CLIENT_NAME"
 
-# Set OpenVPN PKI directory
-OVPN_PKI_DIR="/etc/openvpn/pki"
+# Determine server's public IP address
+SERVER_IP=$(curl -s ifconfig.me)
+
+# Determine OpenVPN port
+OVPN_PORT=$(grep -w "^port" "$OVPN_DIR/server.conf" | awk '{print $2}')
 
 # Create client configuration file
 cat << EOF > "$CLIENT_NAME.ovpn"
 client
 dev tun
 proto udp
-remote $SERVER_IP $SERVER_PORT
+remote $SERVER_IP $OVPN_PORT
 resolv-retry infinite
 nobind
 persist-key
@@ -46,19 +48,19 @@ cipher AES-256-CBC
 auth SHA256
 verb 3
 <ca>
-$(cat "$OVPN_PKI_DIR/ca.crt")
+$(cat "$OVPN_DIR/pki/ca.crt")
 </ca>
 <cert>
-$(sed -ne '/BEGIN CERTIFICATE/,$ p' "$OVPN_PKI_DIR/issued/$CLIENT_NAME.crt")
+$(sed -ne '/BEGIN CERTIFICATE/,$ p' "$OVPN_DIR/pki/issued/$CLIENT_NAME.crt")
 </cert>
 <key>
-$(cat "$OVPN_PKI_DIR/private/$CLIENT_NAME.key")
+$(cat "$OVPN_DIR/pki/private/$CLIENT_NAME.key")
 </key>
 <tls-auth>
-$(cat "$OVPN_PKI_DIR/ta.key")
+$(cat "$OVPN_DIR/ta.key")
 </tls-auth>
 <dh>
-$(cat "$OVPN_PKI_DIR/dh.pem")
+$(cat "$OVPN_DIR/dh.pem")
 </dh>
 EOF
 
